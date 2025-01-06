87a3c1ae50e433a63ba7145dc6c5c477e81549a9
Diegormartins84 <diegormartins84@gmail.com>
2025-01-05 22:47:54 -0300

    Update EsquemaRelacional-Ecommerce.sql

--- EsquemaRelacional-Ecommerce.sql
+++ EsquemaRelacional-Ecommerce.sql
@@ -9,7 +9,7 @@ create table Clients(
 	IdClient int auto_increment primary key,
     Fname varchar(10),
     Minit char(3),
-    Lname varchar(20),
+    Lname varchar(20), 
     CPF char(11) not null,
     Address varchar(255),
 	constraint unique_cpf_client unique (CPF)
